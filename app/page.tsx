a3001a1c568686f3088e8caec6683b0c5835663b
martin9020 <8636852+martin9020@users.noreply.github.com>
2024-02-25 12:34:54 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -26,16 +26,15 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-      `<!doctype html><html><head>
-<title>Game of Life Frame</title>
-<meta property="fc:frame" content="vNext" />
-<meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmQYrWmN73VXtV4ShiDfWsFrzkT6FinvAkVpvaop9HnohD/5.png" />
-<meta property="og:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmQYrWmN73VXtV4ShiDfWsFrzkT6FinvAkVpvaop9HnohD/5.png" />
-<meta property="fc:frame:button:1" content="Start" />
-<meta property="fc:frame:button:2" content="Pause" />
-<meta property="fc:frame:button:3" content="Reset" />
-<meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/gameoflife.js" />
-    
+    <>
+      <title>Game of Life Frame</title>
+      <meta property="fc:frame" content="vNext" />
+      <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmQYrWmN73VXtV4ShiDfWsFrzkT6FinvAkVpvaop9HnohD/5.png" />
+      <meta property="og:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmQYrWmN73VXtV4ShiDfWsFrzkT6FinvAkVpvaop9HnohD/5.png" />
+      <meta property="fc:frame:button:1" content="Start" />
+      <meta property="fc:frame:button:2" content="Pause" />
+      <meta property="fc:frame:button:3" content="Reset" />
+      <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/gameoflife.js" />
       <h1><iframe src="https://miv.wtf"> </iframe></h1>`
     </>
   );
